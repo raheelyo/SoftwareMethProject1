967c93fab63a274e07f4c5f10eef18ee5157571a
Eliza Nieves <eliza.nieves@rutgers.edu>
2020-02-09 01:06:35 -0500

    basic structure for team, team member, and date

--- TeamMember.java
+++ TeamMember.java
@@ -9,21 +9,31 @@ public class TeamMember
    
    public TeamMember(String nm, Date date)
    {
-       
+      this.name = nm;
+      this.startDate = date;
    }
    
    public Date getStartDate()
    {
-      
+      return this.startDate;
    }
    
    public boolean equals(Object obj)
    {
+      if(!(obj instanceof TeamMember)){
+         return false;
+      }
+      TeamMember test = (TeamMember) obj;
+      if(test.name != this.name || test.startDate != this.startDate){
+         return false;
+      }
+      return true;
       //name and startDate must be the same
    }  
    
    public String toString()
    {
+      return this.name + " " + this.startDate;
        //name + " " + startDate;
    }
 
@@ -31,5 +41,5 @@ public class TeamMember
    {
       //testbed main; you must include test cases that exercise 
       //the constructor and all methods in this class.
-   }
+   }//TODO;
 }
