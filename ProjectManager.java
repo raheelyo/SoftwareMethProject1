58ca8fe841b1a2f147a86f10c2ca6dd44fb04bbb
Mohammad <mohammad@Viveks-MBP.hsd1.nj.comcast.net>
2020-02-09 22:02:27 -0500

    Finished ProjectManager.java and TeamMember.java testbed main function.

--- ProjectManager.java
+++ ProjectManager.java
@@ -1,54 +1,88 @@
 /**
-  
- @author  
+
+ @author
  */
- 
+
 public class ProjectManager
 {
    Scanner stdin;
    Team cs213;
    public void run()
    {
-       
+
       boolean done = false;
       while ( !done )
       {
          String command = stdin.next();
-         switch (                )  
-         {   
-		 case 'A': add();
-		      break; 
-		 case 'P': print();
-            	      break;
-		 case 'R': remove();              
-		      break;
-		 case 'Q': print();
-	 	      System.out.println("The team is ready to go!");
-		      break;
-		 default: //deal with bad command here 
-         }  
+         switch (                )
+         {
+           case 'A': add();
+           break;
+
+           case 'P': print();
+           break;
+
+           case 'R': remove();
+           break;
+
+           case 'Q': print();
+           System.out.println("The team is ready to go!");
+           done = true;
+           break;
+
+           default: System.out.println("Command '" + command + "' is not supported!") //deal with bad command here
+         }
       }
       //write java code before you terminate the program
    } //run()
-   
+
    private void add()
    {
-	   
-	   TeamMember currName = new TeamMember(stdin.next(),stdin.next());
-	   
-      	//must check if the date is valid
-	//must call the contains() method to check if a given 
-	//team member is in the team already
+     String currName = stdin.next();
+     Date currDate = new Date(stdin.next());
+     //check first if date is valid
+     if(!currDate.isValid()){
+       System.out.println(currDate.toString() + " is not a valid date!");
+       return;
+     }
+     //now check if name/start date exists
+     TeamMember newMember = new TeamMember(currName,currDate);
+     // memberExists = cs213.find(newMember);
+     if(cs213.find(newMember) == -1){
+       cs213.add(newMember);
+       System.out.println(newMember.print().toString());
+     }else{
+       System.out.println(newMember.toString() + " is already in the team.")
+     }
    }
-   
+
    private void remove()
    {
+     String currName = stdin.next();
+     Date currDate = new Date(stdin.next());
       //must check if the date is valid
-	   
+      if(!currDate.isValid()){
+        System.out.println(currDate.toString() + " is not a valid date!");
+        return;
+      }
+      //now check if member exists in team
+      TeamMember member = new TeamMember(currName,currDate);
+      if(cs213.find(newMember) == -1){
+        System.out.println(member.toString() + " is not a team member.");
+      }else{
+        cs213.remove(member);
+        System.out.println(member.toString() + " has left the team.");
+      }
+
    }
-   
+
    private void print()
    {
-      //must check if the team has 0 members. 
-   }   
+     if(cs213.isEmpty()){
+       System.out.println("We have 0 team members!");
+     }else{
+       cs213.print();
+     }
+      //must check if the team has 0 members.
+   }
 } //ProjectManager
--- TeamMember.java
+++ TeamMember.java
@@ -1,23 +1,23 @@
 /**
- 
- @author  
+
+ @author
  */
-public class TeamMember 
+public class TeamMember
 {
    private String name;
    private Date   startDate;
-   
+
    public TeamMember(String nm, Date date)
    {
       this.name = nm;
       this.startDate = date;
    }
-   
+
    public Date getStartDate()
    {
       return this.startDate;
    }
-   
+
    public boolean equals(Object obj)
    {
       if(!(obj instanceof TeamMember)){
@@ -29,8 +29,8 @@ public class TeamMember
       }
       return true;
       //name and startDate must be the same
-   }  
-   
+   }
+
    public String toString()
    {
       return this.name + " " + this.startDate;
@@ -39,7 +39,33 @@ public class TeamMember
 
    public static void main(String [] args)
    {
-      //testbed main; you must include test cases that exercise 
+     TeamMember t1 = new TeamMember("John","02/28/2019"); //check constructor
+     TeamMember t2 = new TeamMember("Wick","03/30/2020");
+     TeamMember t3 = new TeamMember("John","02/28/2019"); //I'm sorry I'm watching John Wick as I'm doing this
+     System.out.println("Test 1: " + t1.toString() + "\n" + "Test 2: " +
+      t2.toString() + "\n" + "Test 3: " t3.toString()); //Check if toString() works
+
+     if(t1.equals(t2)){ //test equals() in negative case
+       System.out.println(t1.toString() + " and " t2.toString() + " are the same.");
+     }else{
+       System.out.println(t1.toString() + " and " t2.toString() + " are not the same.");
+     }
+     if(t1.equals(t3)){ //test equals() in positive case
+       System.out.println(t1.toString() + " and " t3.toString() + " are the same.");
+     }else{
+       System.out.println(t1.toString() + " and " t3.toString() + " are not the same.");
+     }
+     String notATeamMember = "John 02/28/2019";
+     if(t1.equals(notATeamMember)){ //check if
+       System.out.println("This is surprising.");
+     }else{
+       System.out.println(t1.toString() + " is a team member variable, but
+        " + notATeamMember + " is a string.");
+     }
+
+     System.out.println(t2.name + "'s start date: " + t2.getStartDate());
+
+      //testbed main; you must include test cases that exercise
       //the constructor and all methods in this class.
    }//TODO;
 }
